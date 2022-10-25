fc887d16b6de2a18c7a253072b15c04ed0175f83
Alexandr Naum <mfq07k9cz@gmail.com>
2022-10-25 11:49:32 +0300

    Component state

--- src/App.js
+++ src/App.js
@@ -4,15 +4,20 @@ import logo from './logo.svg'
 import './App.css'
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      name: 'Alex'
+    }
+  }
+
   render() {
     return (
       <div className='App'>
         <header className='App-header'>
           <img src={logo} className='App-logo' alt='logo' />
-          <p>Hola!</p>
-          <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
-            Learn React
-          </a>
+          <p>Hi {this.state.name}!</p>
+          <button>Change Name</button>
         </header>
       </div>
     )
